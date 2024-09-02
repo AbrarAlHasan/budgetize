dec417527ce3c59c5e726540d23a6cd692cee45e
Abrar <abraralhasan111@gmail.com>
2024-09-02 10:37:58 +0530

    Calander date Range Select For Current Month

--- utils/DateCalculator.ts
+++ utils/DateCalculator.ts
@@ -1,13 +1,16 @@
 import moment from "moment";
 
 export const getCurrentWeekRange = (customDate?: Date) => {
-  console.log("CUSTOM DATE",customDate);
-  const fromDate = moment(customDate ?? new Date())
-    .startOf("weeks")
-    .toDate();
-  const toDate = moment(customDate ?? new Date())
-    .endOf("weeks")
-    .toDate();
+  console.log("CUSTOM DATE", customDate);
+  const date = moment(customDate);
+  let fromDate = date.clone().startOf("week").toDate();
+  let toDate = date.clone().endOf("week").toDate();
+
+  const startOfMonth = date.clone().startOf("month").toDate();
+  const endOfMonth = date.clone().endOf("month").toDate();
+
+  fromDate = fromDate < startOfMonth ? startOfMonth : fromDate;
+  toDate = toDate > endOfMonth ? endOfMonth : toDate;
 
   return {
     fromDate: fromDate,
