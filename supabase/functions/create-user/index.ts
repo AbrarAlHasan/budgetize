e6beef55c1724b14180266b23501524084667732
Abrar <abraralhasan111@gmail.com>
2024-11-07 00:15:43 +0530

    create user padding remove

--- supabase/functions/create-user/index.ts
+++ supabase/functions/create-user/index.ts
@@ -181,6 +181,11 @@ Deno.serve(async (req) => {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Temporary Password</title>
   <style>
+  * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+    }
     body {
       font-family: Arial, sans-serif;
       color: #333;
