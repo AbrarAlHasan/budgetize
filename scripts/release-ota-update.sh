a27eb0f7b6e3ad59aba380fdc0ac4d66a38348dc
Abrar Hasan <abraralhasan111@gmail.com>
2025-12-12 11:16:59 +0530

    Update release message in release-ota-update.sh to reflect new auto-focus functionality for amount input

--- scripts/release-ota-update.sh
+++ scripts/release-ota-update.sh
@@ -34,7 +34,7 @@ fi
 export EXPO_TOKEN
 
 # Update message (change this for each release)
-UPDATE_MESSAGE="Default Tags Applied Reset Bug Fixing"
+UPDATE_MESSAGE="auto-focus functionality for the amount input"
 
 npx eas-cli whoami
 
